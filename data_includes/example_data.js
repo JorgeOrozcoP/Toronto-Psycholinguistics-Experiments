d4c59fda6746f0901503923387b0966bef33b044
Jorge Orozco <yorchorozco@gmail.com>
2019-04-14 18:22:43 +0200

    error h3

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -166,7 +166,7 @@ var items = [
           <td> <input type="radio" name="dog" value="opt2"> </td> \
       <td> <input type="radio" name="dog" value="opt3"> </td> \
         </tr>'},
-      {continueMessage: "Klicken Sie hier"}
+      continueMessage: "Klicken Sie hier"
         ],
 		
 		
